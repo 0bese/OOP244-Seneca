be05d1a21126c6c3ed208851411d91cadbbd90fe
Kojo Anyane Obese <obesean@icloud.com>
2023-11-06 15:32:19 -0500

    completed ws06

--- WS06/lab/HealthCard.cpp
+++ WS06/lab/HealthCard.cpp
@@ -7,9 +7,7 @@ using namespace std;
 namespace sdds {
     //Checks the validity of Health Card information
     bool HealthCard::validID(const char* name, long long number, const char vCode[], const char sNumber[]) const {
-        return name != nullptr && strlen(name) > 0 &&
-            number > 999999999 && number < 9999999999 &&
-            strlen(vCode) == 2 && strlen(sNumber) == 9;
+        return name && name[0] && number > 999999999 && number < 9999999999 && strlen(vCode) == 2 && strlen(sNumber) == 9;
     }
 
     //Deallocate memory and set m_name to NULL
@@ -42,15 +40,14 @@ namespace sdds {
 
     //Sets the health card information
     void HealthCard::set(const char* name, long long number, const char vCode[], const char sNumber[]) {
+        delete[] m_name;
+        m_name = nullptr;
+        setEmpty();
         if (validID(name, number, vCode, sNumber)) {
             allocateAndCopy(name);
             m_number = number;
             strcpy(m_vCode, vCode);
             strcpy(m_sNumber, sNumber);
-        }
-        else {
-            delete[] m_name;
-            setEmpty();
         }
     }
 
@@ -60,10 +57,12 @@ namespace sdds {
         set(name, number, vCode, sNumber);
     }
 
+
     //Copy Constructor
     HealthCard::HealthCard(const HealthCard& hc) {
-        set(hc.m_name, hc.m_number, hc.m_vCode, hc.m_sNumber);
+        *this = hc;
     }
+
 
     //Assignment operator
     HealthCard& HealthCard::operator=(const HealthCard& hc) {
@@ -86,35 +85,31 @@ namespace sdds {
 
     //Print health card information
     ostream& HealthCard::print(ostream& ostr, bool toFile) const {
-        if (validID(m_name, m_number, m_vCode, m_sNumber)) {
-
+        if (*this) {
             if (toFile) {
-                // Print the HealthCard information with comma separators for file output
-                ostr << m_name << ",";
+                ostr << m_name;
+                ostr << ',';
                 printIDInfo(ostr);
                 ostr << endl;
             }
-
             else {
-                // Format and print the HealthCard information with a left-aligned 50-character width
-                ostr.setf(ios::left);
-                ostr.fill('.');
                 ostr.width(50);
+                ostr.fill('.');
+                ostr.setf(ios::left);
                 ostr << m_name;
                 printIDInfo(ostr);
             }
         }
+
         return ostr;
     }
 
     //Reads Health Card information from input stream
     istream& HealthCard::read(istream& istr) {
-        char name[MaxNameLength + 1];
-        long long number;
-        char vCode[3];
-        char sNumber[10];
-
-        // Read the values from the input stream 'istr'
+        char name[MaxNameLength]{ '\0' };
+        long long number{ 0 };
+        char vCode[3]{ 0 };
+        char sNumber[10]{ 0 };
         istr.get(name, MaxNameLength, ',');
         extractChar(istr, ',');
         istr >> number;
@@ -122,26 +117,26 @@ namespace sdds {
         istr.get(vCode, 3, ',');
         extractChar(istr, ',');
         istr.get(sNumber, 10, '\n');
-
-        // If reading was successful, set the HealthCard values
-        if (!istr.fail())  {
+        extractChar(istr, '\n');
+        if (istr) {
             set(name, number, vCode, sNumber);
         }
-
-        // Clear any error flags, and ignore any remaining input up to the newline character
-        istr.clear();
-        istr.ignore(1000, '\n');
+        else {
+            istr.clear();
+            istr.ignore(1000, '\n');
+        }
         return istr;
     }
 
     //Overloaded output operator to print Health Card
     ostream& operator<<(ostream& ostr, const HealthCard& hc) {
-        if (bool(hc)) {
+        if (hc) {
             hc.print(ostr, false);
         }
         else {
             ostr << "Invalid Health Card Record";
         }
+
         return ostr;
     }
 
